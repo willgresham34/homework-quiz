8fb8ee59ef5029dabd7dc91549058f038e3c7566
Will Gresham <willgresham@Wills-MacBook-Air.local>
2021-09-20 20:42:44 -0400

    Finished question conatiner design

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+// questions
+
+// 1. What Does HTML Stand For ?
+// Hyper Text Markup Link
+// Hyper Talk Markdown Language
+// Hyper Text Markup Language
+// Hyper Text Measure Link
+
+// 2. Commonly used Data types do not include
+// strings
+// booleans
+// alerts
+// numbers
+
+// 3.  Different types of windows dont include
+// alert
+// prompt
+// confirm
+// contact
+
+// 4. Which of the following is a self closing tag in HTML
+// <p>
+// <h1>
+// <link>
+// <span>
+
+// 5.  What do you use to identify a class in css?
+// #
+// .
+// Just its name 
+// :
