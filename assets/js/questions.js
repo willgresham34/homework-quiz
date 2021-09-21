1502e404163011897957e688b243fda7e07df2c2
Will Gresham <willgresham@Wills-MacBook-Air.local>
2021-09-21 18:41:58 -0400

    Questions Array

---
+++ assets/js/questions.js
@@ -0,0 +1,62 @@
+
+var questions = [
+{
+
+numb: 1,
+question: 'What Does HTML Stand For ?',
+answer: "Hyper Text Markup Language",
+options: [
+ 'Hyper Text Markup Link',
+ 'Hyper Talk Markdown Language',
+ 'Hyper Text Markup Language',
+ 'Hyper Text Measure Link',
+    ]
+}, 
+    {
+    numb: 2,
+    question: 'Commonly used Data types do not include?',
+    answer: 'alerts',
+    options: [
+    'strings',
+    'booleans',
+    'alerts',
+    'numbers',
+    ]
+
+},
+    {
+    numb: 3,
+    question: "Different types of windows dont include",
+    answer: 'contact',
+    options: [
+    'alert',
+    'prompt',
+    'confirm',
+    'contact',
+    ]
+},
+    {
+    numb: 4,
+    question: "Which of the following is a self closing tag in HTML",
+    answer: "<link>",
+    options: [
+    '<p>',
+    '<h1>',
+    '<link>',
+    '<span>',
+    ]
+},
+    {
+    numb: 5,
+    question: 'What do you use to identify a class in css?',
+    answer: ".",
+    options: [
+    '#',
+    '.',
+    'Just its name', 
+    ':',
+    ]
+}
+
+
+];
